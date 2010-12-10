db6ba82af0dac4ab947f288c32a2d53cd9325b80
Marc Kirchner <mail@marc-kirchner.de>
2010-12-10 17:23:01 -0500

    commiting first version of the MS2 example

---
+++ examples/example-ms2-ms1-matching.cpp
@@ -0,0 +1,325 @@
+/* 
+ * example-ms2-ms1-matching.cpp
+ *
+ * Copyright (c) 2010 Marc Kirchner <marc.kirchner@childrens.harvard.edu>
+ * Copyright (c) 2010 Buote Xu <buote.xu@gmail.com>
+ *
+ * This file is part of libfbi.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without  restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions: 
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef __LIBFBI_EXAMPLES_EXAMPLEMS2MS1MATCHING_H__
+#define __LIBFBI_EXAMPLES_EXAMPLEMS2MS1MATCHING_H__
+
+#include <string>
+#include <tuple>
+#include <utility>
+#include "fbi/tuplegenerator.h"
+
+#include <iostream>
+#include <fstream>
+#include <tuple>
+#include <string>
+#include <sys/time.h>
+#include <vector>
+#include <boost/program_options.hpp>
+
+#include "fbi/tuplegenerator.h"
+#include "fbi/fbi.h"
+#include "fbi/connectedcomponents.h"
+/*
+ * User classes
+ */
+struct Xic
+{
+    double rt_;
+    double mz_;
+    double abundance_;
+    Xic(const double &rt, const double& mz, const double & abundance) 
+      : rt_(rt), mz_(mz), abundance_(abundance) {}
+};
+
+struct Peptide
+{
+    double rt_;
+    double mz_;
+    std::string sequence_;
+    Peptide(const double &rt, const double& mz, const std::string& seq)
+      : rt_(rt), mz_(mz), sequence_(seq) {}
+};
+
+/*
+ * Type traits info for libfbi
+ */
+namespace fbi {
+
+template<> struct Traits<Xic> : mpl::TraitsGenerator<double, double> {};
+template<> struct Traits<Peptide> : mpl::TraitsGenerator<double, double> {};
+
+} 
+
+/*
+ * Xic adapters
+ */
+struct XicBoxGenerator
+{
+  template <size_t N>
+  typename std::tuple_element<N, 
+    typename fbi::Traits<Centroid>::key_type>::type 
+  get(const Xic &) const;
+
+  double fullScanPpm_;
+  double rtWindow_;
+
+  BoxGenerator(double fullscanPpm, double rtWindow_)
+    : fullScanPpm_(fullScanPpm), rtWindow_(rtWindow)
+  {}
+};
+
+template <>
+std::pair<double, double>  
+XicBoxGenerator::get<0>(const Xic& xic) const 
+{
+  return std::make_pair(
+    xic.mz_* (1 - fullScanPpm_ * 1E-6), 
+    xic.mz_* (1 + fullScanPpm_ * 1E-6));
+}
+
+template <>
+std::pair<double, double>  
+XicBoxGenerator::get<1>(const Xic & xic) const
+{
+  return std::make_pair(xic.rt_ - rtWindow_, xic.rt_ + rtWindow_);
+}
+
+/*
+ * Peptide classes and adapters
+ */
+struct PeptideBoxGenerator
+{
+  template <size_t N>
+  typename std::tuple_element<N, 
+    typename fbi::Traits<Centroid>::key_type>::type 
+  get(const Peptide &) const;
+
+  double preScanPpm_;
+  double rtWindow_;
+
+  BoxGenerator(double prescanPpm, double rtWindow_)
+    : preScanPpm_(preScanPpm), rtWindow_(rtWindow)
+  {}
+};
+
+template <>
+std::pair<double, double>  
+PeptideBoxGenerator::get<0>(const Peptide& peptide) const 
+{
+  return std::make_pair(
+    peptide.mz_* (1 - preScanPpm_ * 1E-6), 
+    peptide.mz_* (1 + preScanPpm_ * 1E-6));
+}
+
+template <>
+std::pair<double, double>  
+PeptideBoxGenerator::get<1>(const Peptide & peptide) const
+{
+  return std::make_pair(peptide.rt_ - rtWindow_, peptide.rt_ + rtWindow_);
+}
+
+/*
+ * load xics from file
+ */
+std::vector<Xic> parseXicFile(ProgramOptions & options)
+{
+  std::vector<Xic> xics;
+  std::ifstream ifs(options.xicFileName_.c_str());
+  ifs.setf(std::ios::fixed, std::ios::floatfield);
+
+  double mz, rt, abundance;
+
+  while (ifs >> rt >> mz >> abundance) {
+    xics.push_back(Xic(rt, mz, abundance));
+  }
+  return xics;
+}
+
+/*
+ * load peptides from file
+ */
+std::vector<Peptide> parsePeptideFile(ProgramOptions & options)
+{
+  std::vector<Peptide> peptides;
+  std::ifstream ifs(options.peptideFileName_.c_str());
+  ifs.setf(std::ios::fixed, std::ios::floatfield);
+
+  double mz, rt;
+  std::string seq;
+
+  while (ifs >> rt >> mz >> seq) {
+    xics.push_back(Peptide(rt, mz, seq));
+  }
+  return xics;
+}
+
+
+/*
+ * store and process user options
+ */
+
+struct ProgramOptions 
+{
+    double prescanPpm_;
+    double fullscanPpm_;
+    double rtWindow_;
+    std::string xicFileName_;
+    std::string peptideFileName_;
+    std::string outputfileName_;
+};
+
+int parseProgramOptions(int argc, char* argv[], ProgramOptions& options)
+{
+  namespace po = boost::program_options;
+  std::string config_file;
+  double pres, fres;
+  po::options_description generic("Generic options");
+  generic.add_options()
+    ("help", "Display this help message")
+    ("config,c", po::value<std::string>(&config_file), "config file")
+    ("xicfile,x", po::value<std::string>(&options.xicFileName_), "input file")
+    ("peptidefile,p", po::value<std::string>(&options.peptideFileName_), "input file")
+    ("outputfile,o", po::value<std::string>(&options.outputFileName_), "output file")
+    ;
+
+  po::options_description config("Allowed options");
+  config.add_options()
+      ("pr", po::value<double>(&pres)->default_value(7500),
+        "The resolution of the prescan.")
+      ("fr", po::value<double>(&fres)->default_value(60000),
+        "The resolution of the full MS1 parent scan.")
+      ("fr", po::value<double>(&options.rtWindow_)->default_value(60),
+        "The size of the retention time search window (in s).");
+
+  po::options_description cmdline_options("Options available via command line");
+  cmdline_options.add(generic).add(config);
+
+  po::options_description config_file_options(
+    "Options available in the config file");
+  config_file_options.add(config);
+
+  po::options_description visible("Allowed options");
+  visible.add(generic).add(config);
+  
+  po::positional_options_description p;
+  p.add("inputfile", -1);
+  
+  po::variables_map vm;
+
+  po::store(po::command_line_parser(argc, argv).options(
+    cmdline_options).positional(p).run(), vm);
+  po::notify(vm);    
+  if (vm.count("config")){ 
+    std::ifstream ifs(config_file.c_str());
+    if (!ifs) {
+      std::cout << "can't open config file: " << config_file << '\n';
+      return -1;
+    } else {
+      std::cerr << "Using config file" << config_file << '\n';
+      po::store(po::parse_config_file(ifs, config_file_options), vm);
+      po::notify(vm);    
+    }
+  }
+
+  if (vm.count("help")) {
+    std::cout << visible << "\n";
+    return -1;
+  }
+  
+  if (vm.count("xicfile")) {
+    std::cerr << "xicfile: " <<
+        options.inputfileName_ << '\n';
+  } else {
+    std::cerr << "XIC file required." << '\n';
+    std::cout << visible << '\n';
+    return -1;
+  }
+
+  if (vm.count("peptidefile")) {
+    std::cerr << "peptidefile: " <<
+        options.inputfileName_ << '\n';
+  } else {
+    std::cerr << "Peptide input file required." << '\n';
+    std::cout << visible << '\n';
+    return -1;
+  }
+
+  if (vm.count("outputfile")) {
+    std::cerr << "outputfile: " <<
+        options.outputfileName_ << '\n';
+  } else {
+    options.outputfileName_ = options.inputfileName_ + std::string(".out");
+    std::cerr << "outputfile: " <<
+        options.outputfileName_ << '\n';
+  }
+
+  options.prescanPpm_ = 1e6 / (pres * (2*std::sqrt(2*std::log(2))));
+  options.fullscanPpm_ = 1e6 / (fres * (2*std::sqrt(2*std::log(2))));
+
+  return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    using namespace fbi;
+
+    ProgramOptions options;
+    if (parseProgramOptions(argc, argv, options) != 0) {
+        return -1;
+    }
+    // load data
+    std::vector<Xic> xics = parseXicFile(options);
+    std::vector<Peptide> peptides = parsePeptideFile(options);
+
+    timeval start, end; 
+    gettimeofday(&start, NULL);
+    auto adjList = SetA<Xic, 0, 1>::SetB<Peptide, 0, 1>::intersect(
+      xics, XicBoxGenerator(options.fullscanPpm_, options.rtWindow_),
+      peptides, PeptideBoxGenerator(options.prescanPpm_, options.rtWindow_))
+    gettimeofday(&end, NULL);
+    std::cout << "fbi runtime: "
+      << static_cast<double>(end.tv_sec - start.tv_sec) +
+         static_cast<double>(end.tv_usec - start.tv_usec)* 1E-6 << '\n'
+
+    std::ofstream ofs(options.outputfileName_.c_str());
+    ofs.setf(std::ios::fixed, std::ios::floatfield);
+    for (size_t i = 0; i < adjList.size(); ++i) {
+        Xic& xic = xics[i];
+        ofs << xic.rt_ << '\t' << xic.mz_ << '\t' << '\t' 
+          << xic.abundance_;
+        if (adjList[i].empty()) {
+            ofs << '\n';
+        } else {
+            for (size_t j = 0; j < adjList.size(); ++j) {
+                ofs << '\t' << peptides[adjList[i][j]].sequence_;
+            }
+            ofs << 'n';
+    }
+    return 0;
+}
+
