4ce156b3e491700e13cb2faa283e7f3ba5d77e68
Marc Kirchner <mail@marc-kirchner.de>
2010-12-20 15:59:34 -0500

    * cleaning out old htdocs (now in gh-pages branch) * adding a target to sync docs w/ the gh-pages branch

---
+++ scripts/sync-docs-gh-pages.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# sync a new build of the docs with the gh-pages branch
+#
+# Copyright (c) 2010 Marc Kirchner
+#
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <libfbi-source-dir> <libfbi-build-dir>"
+    exit 1
+fi
+
+# the sync target depends on the doc target, hence the doc path should 
+# be availale
+if [ ! -f $2/doc/html/index.html ]; then
+    echo "Please build the doc target before attempting to sync."
+    exit 1
+fi
+
+# attempt to switch branches
+( cd $1 ; git checkout gh-pages )
+
+# make sure we are on gh-pages
+if [ `( cd $1; git branch ) | grep '*' | cut -f2 -d' ' | grep '^gh-pages$' | wc -l` -lt 1 ] ; then
+    echo "Switching branches failed. Aborting."
+    exit 1
+fi
+
+# get a copy of the docs
+( cd $2 ; tar cf - doc | ( cd $1 ; tar xvf - ) )
+
+# commit the change
+#( cd $1 ; git commit -a -m "doc update" )
+
+# switch back to master branch
+#( cd $1 ; git checkout master )
+
+
+
