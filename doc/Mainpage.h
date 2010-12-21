c4a92ff19dc0c5b2411bc1f78ecfb62672a8aad1
Marc Kirchner <mkirchner@rikku.tch.harvard.edu>
2010-12-21 18:41:32 -0500

    documenting cmake configuration to use libfbi

--- doc/Mainpage.h
+++ doc/Mainpage.h
@@ -5,9 +5,33 @@
 
 
 \section sec_intro Introduction
-\c libfbi computes intersections between two sets of boxes. This is an important
-task in many data analysis algorithms that require e.g. neighborhood information
-(in the form of an adjacency list) for all points in large datasets.
+
+\c libfbi is a header-only C++ template library that enables the efficient
+solution of box intersection problems in an arbitrary number of dimensions. The
+implementation is based on a publication by Zomorodian and Edelsbrunner (2000)
+and makes heavy use of C++ metaprogramming and variadic template programming
+techniques. Despite this complexity, the library provides a straightforward and
+simple interface that allows easy integration into new and existing projects. 
+
+libfbi has so far been applied to feature extraction and data analysis tasks
+for high-resolution liquid chromatorgraphy/mass spectrometry (LC/MS) data sets.
+The library itself, however, is in no way limited to this application scenario:
+it can easily be used to tackle many kinds of multi-dimensional box
+intersection and related problems (e.g. determining bounding volumes,
+k-nearest-neighbor search, density estimation, correspondence estimation in
+motion or pattern alignment tasks, etc.).
+
+\section sec_license Licensing
+\c libfbi is available under an MIT license. In essence, this means that
+everybody, in particular including all academic institutions and companies are
+expliciltly allowed to make use of \c libfbi, as long as the original
+disclaimers and copyrights are left untouched. You are explicitly allowed to
+incorporate \c libfbi into new or existing propietary software products and you
+are not required to disclose any source code that uses the library or any
+changes you make. For details, see the license
+text available at the top of each header file and in LICENSE.txt.
+
+That said, your bug reports, suggestions and bug fixes are appreciated.
 
 \section sec_citation Citation
 Details concerning the \c libfbi implementation are available from two principal
@@ -26,19 +50,34 @@ GCC 4.4, on an Ubuntu Linux system. Windows users currently need to make use of
 the MinGW compiler as variadic template support under Microsoft Visual Studio
 will only be available starting from Visual Studio 11.
 
+\c libfbi itself is free of any third-party dependencies; the tests and
+examples make use of the VIGRA \c unittest header file and depend on the BOOST
+library. Memory tests require \c valgrind, coverage analysis depends on \c
+gcov. 
+
 \section sec_install Installation
 \c libfbi is a header-only library and does not require the installation of any
-compiled libraries. 
+compiled libraries. To use libfbi, 
+\li download a header file and documentation bundle, 
+\li include the header files in your project and
+\li adjust the compilation flags of your compiler (\c libfbi requires the 
+\c -std=c++0x flag in gcc).
 
-\subsection sec_install_src Distribution Packages
-\c libfbi distribution packages for are available for download from 
-the \c libfbi project page at http://software.steenlab.org/libfbi.
-To install, simply run the installer and ensure that the \c libfbi include
-directory is in the include search path.
+If you use the \c CMake build system, include the following lines into your
+\c CMakeLists.txt to use \c libfbi:
+\verbatim
+[...]
+find_package(LIBFBI)
+if (LIBFBI_FOUND)
+    include (${LIBFBI_USE_FILE})
+    add_executable(MyProgram MyProgram.cpp)
+endif(LIBFBI_FOUND)
+[...]
+\endverbatim
 
 \subsection sec_install_src Building from Source
-Because \c libfbi is a header-only library, there is no need to build the
-library inorder to use it in software projects. 
+Because \c libfbi is a header-only library, there is no need to build any
+library inorder to use it in software projects.
 The following illustrates how to build the examples, tests and
 distribution packages. Bundling \c libfbi requires a working CMake build system
 (available from http://cmake.org/) and CMake >= 2.6.
@@ -48,7 +87,7 @@ With cmake in the system path, the build process is
  tar xvzf libfbi-xxxxxxx.tar.gz
  mkdir libfbi-build
  cd libfbi-build
- cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_TESTING=TRUE ../libfbi
+ ccmake ../libfbi
  make
  make test
 \endverbatim
